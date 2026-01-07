c927b4ed4781a1cffcca45e3beeffb210aa1e1a1
Yaoyuxuanyyds <1282557919@qq.com>
2026-01-07 08:44:51 +0000

    update

--- compute_sd3_text_procrustes.sh
+++ compute_sd3_text_procrustes.sh
@@ -4,5 +4,4 @@ python compute_sd3_text_procrustes.py \
   --num-samples 50000 \
   --origin-layer 1 \
   --target-layer-start 2 \
-  --output /inspire/hdd/project/chineseculture/public/yuxuan/Training-free-Residual-SD3/logs/procrustes_rotations/procrustes_rotations_blip3o50k_ln.pt \
-  --dataset-train \
+  --output /inspire/hdd/project/chineseculture/public/yuxuan/Training-free-Residual-SD3/logs/procrustes_rotations/procrustes_rotations_coco5k_ln.pt \
