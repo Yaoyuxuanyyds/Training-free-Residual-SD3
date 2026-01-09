68d3c37057391dc2b98199b6c480f8b0737a07ae
Yaoyuxuanyyds <1282557919@qq.com>
2026-01-09 07:07:26 +0000

    fix qwenimage

--- Qwen-Image-Residual/compute_qwen_text_procrustes.sh
+++ Qwen-Image-Residual/compute_qwen_text_procrustes.sh
@@ -2,7 +2,7 @@ python compute_qwen_text_procrustes.py \
   --model /inspire/hdd/project/chineseculture/public/yuxuan/base_models/Diffusion/Qwen-Image \
   --dataset blip3o60k \
   --datadir /inspire/hdd/project/chineseculture/public/yuxuan/datasets \
-  --num_samples 50000 \
+  --num_samples 5000 \
   --origin_layer 1 \
   --target_layer_start 2 \
   --output /inspire/hdd/project/chineseculture/public/yuxuan/Qwen-Image-Residual/output/procrustes_rotations/qwen_procrustes_rotations.pt
