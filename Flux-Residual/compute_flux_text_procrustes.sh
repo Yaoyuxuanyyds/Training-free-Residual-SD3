f45e9b6237cf6c3ad1a039492e2a38727c48f3be
Yaoyuxuanyyds <1282557919@qq.com>
2026-01-20 03:09:08 +0000

    update

--- Flux-Residual/compute_flux_text_procrustes.sh
+++ Flux-Residual/compute_flux_text_procrustes.sh
@@ -2,9 +2,9 @@ CUDA_VISIBLE_DEVICES=0 python compute_flux_text_procrustes.py \
   --dataset blip3o60k \
   --datadir /inspire/hdd/project/chineseculture/public/yuxuan/datasets \
   --model /inspire/hdd/project/chineseculture/yaoyuxuan-CZXS25220085/p-yaoyuxuan/REPA-SD3-1/flux/FLUX.1-dev \
-  --num-samples 50 \
+  --num-samples 5000 \
   --origin-layer 1 \
   --target-layer-start 2 \
-  --output /inspire/hdd/project/chineseculture/public/yuxuan/Training-free-Residual-SD3/Flux-Residual/logs/procrustes_rotations/procrustes_rotations_coco5k_ln_t1-full.pt \
+  --output /inspire/hdd/project/chineseculture/public/yuxuan/Training-free-Residual-SD3/Flux-Residual/logs/procrustes_rotations/procrustes_rotations_coco5k_ln_t5-full.pt \
   --col-center \
-  --timestep-buckets 1 \
+  --timestep-buckets 5 \
